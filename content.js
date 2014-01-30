d98ac5f2c0ef740912381d94b1d88415c68b8cd8
Cecy Urbina <ceciliaurbina29@gmail.com>
2014-01-29 21:46:35 -0600

    STATIC sidebar vertical added

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ $('div.s9hl').mouseover(function() {
   content = $(this).find('a').attr("title");
   content = content + '<br><br><span class="label label-danger">Reciclable</span>';
   content = content + '<span class="label label-success">Reutilizable</span>';
-  content = content + '<span class="label label-danger">Biodegradable</span>'
+  content = content + '<span class="label label-danger">Biodegradable</span>';
   $(this).popover({
     trigger: 'hover', 
     placement : 'top',
@@ -12,8 +12,8 @@ $('div.s9hl').mouseover(function() {
     title : 'Información ecológica del producto',
     content : content
   });
-
-    $(this).css('border','1px solid #f3f3f3');
+  
+  $(this).css('border','1px solid #f3f3f3');
 });
 
 
@@ -21,18 +21,25 @@ $('div.s9hl').mouseout(function() {
   $(this).css('border','0px solid #f3f3f3');
 });
 
+/*
 var sidebar;
 $('body').css({
-    'padding-right': '350px'
+  'padding-right': '350px'
 });
 sidebar = $("<div id='sidebar'></div>");
 sidebar.css({
-    'position': 'fixed',
-    'right': '0px',
-    'top': '0px',
-    'z-index': 9999,
-    'width': '290px',
-    'height': '100%',
-    'background-color': 'blue'  // Confirm it shows up
+  'position': 'fixed',
+  'right': '0px',
+  'top': '0px',
+  'z-index': 9999,
+  'width': '290px',
+  'height': '100%',
+  'background-color': 'blue'  // Confirm it shows up
 });
+
+button = $("<button type='button' class='btn btn-default'>Left</button>");
+
 $('body').append(sidebar);
+$('body').append(button);
+
+*/
