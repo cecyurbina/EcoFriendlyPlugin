95237299fd18ad23775016e7e8a7a3cba25bdf5b
Cecy Urbina <ceciliaurbina29@gmail.com>
2014-02-27 20:12:04 -0600

    locals js files added

---
+++ content.js
@@ -0,0 +1,23 @@
+console.log("hola");
+$('div.carR2').attr("data-toggle", "popover");
+
+$('div.carR2').mouseover(function() {
+  content = $(this).find("font").first().text();;
+  content = content + '<br><br><span class="label label-danger">Reciclable</span>';
+  content = content + '<span class="label label-success">Reutilizable</span>';
+  content = content + '<span class="label label-danger">Biodegradable</span>';
+  $(this).popover({
+    trigger: 'hover', 
+    placement : 'top',
+    html: true,
+    title : 'Información ecológica',
+    content : content
+  });
+  
+  $(this).css('border','1px solid #f3f3f3');
+});
+
+
+$('div.carR2').mouseout(function() {
+  $(this).css('border','0px solid #f3f3f3');
+});
