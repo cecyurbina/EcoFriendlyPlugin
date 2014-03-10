8dbfdfe3d120ed7e31e46c09332f8f9cc321631f
Cecy Urbina <ceciliaurbina29@gmail.com>
2014-03-10 15:08:42 -0600

    rails server

---
+++ catalogo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catalogo::Application.config.secret_key_base = 'd7e265ca68a37d8acfa19f7d6f8d57d17efc1f5b7e20a637e902807da2f717539652a073f67f698a4125e787704ae0a36e046bbcdb098e67d6fce820778fa92e'
