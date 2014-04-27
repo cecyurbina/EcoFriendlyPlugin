a221fd429123b788d91a9b7bc4aedfe521d05166
Cecy Urbina <ceciliaurbina29@gmail.com>
2014-04-27 14:12:18 -0800

    save in db save data to database by parameters in URL

--- Products/main.py
+++ Products/main.py
@@ -1,16 +1,23 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+
+
+class Product(db.Model):
+  name = db.StringProperty(required=True)
+
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        to = (self.request.get_all("to"))[0]
-        content = (self.request.get_all("content"))[0]
-        self.response.out.write('Para: ' + to)
-        self.response.out.write('<br>Correo enviado')
+        product = (self.request.get_all("product"))[0]
+        self.response.out.write('Product name: ' + product)
+        p = Product(name=product)
+        p.put()
 
 
 app = webapp.WSGIApplication([('/', MainHandler)],
                              debug=True)
+
 
 def main():
     app = webapp.WSGIApplication([('/', MainHandler)],
