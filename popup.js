586af0e393fcff93f414da47fd22b942b235b3fe
Cecy Urbina <ceciliaurbina29@gmail.com>
2014-05-26 23:57:42 -0500

    HTML5 LOCATION added

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
 // found in the LICENSE file.
+var x=document.getElementById("one");
+getLocation();
+function getLocation()
+{
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else{
+    x.innerHTML="Geolocation is not supported by this browser.";
+    alert("no");
+      }
+}
+function showPosition(position)
+{
+  alert("si");
+  x.innerHTML="Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
